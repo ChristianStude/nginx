7f51a522304b885e962474c8bc24d0276893510f
Christian Stude <christian.stude@googlemail.com>
2021-08-25 14:49:11 +0200

    kleine Änderungen

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,6 +6,4 @@ export DEBIAN_FRONTEND=noninteractive
 # System aktualisieren
 apt-get update && apt-get -y dist-upgrade && apt-get --purge -y autoremove
 
-# Tastaturbelegung auf Deutsch umstellen
-dpkg-reconfigure keyboard-configuration && loadkeys de
  
